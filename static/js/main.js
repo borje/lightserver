bc1172673a966d636db3542a6cb200ef93641ca2
Granberg B rje <borje.granberg@saabgroup.com>
2015-09-04 12:49:45 +0200

    Show current event queue on start page

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
     $.getJSON("info", function(data) {
 
@@ -6,5 +5,24 @@ $(document).ready(function() {
         $("#buildtime").append(data.buildTime);
     });
 
+	$.getJSON("status", function(data) {
+		var tableContent = ""
+		$.each(data, function (i, obj) {
+			var date = new Date(obj.Time);
+			var dateString = "";
+			if (!isToday(date)) {
+				dateString = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " ";
+			}
+			dateString += " " + date.getHours() + ":" + date.getMinutes();
+			tableContent += "<tr><td>" + dateString + "</td><td>" + obj.Device+ "</td>";
+			tableContent += "<td>" + (obj.Action == 1 ? "Off" : "On") + "</td></tr>";
+		});
+		$("#queue").append(tableContent);
+
+	}); 
 });
 
+function isToday(date) {
+	var today = new Date();
+	return today.getDate() == date.getDate();
+}
